d2d38bb50ca387dffaae8475b1d655c95ae07804
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-04-11 07:17:47 +0200

    Update App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './contianers/App';
 
 test('renders learn react link', () => {
   render(<App />);
---
+++ src/contianers/App.js
@@ -0,0 +1,5 @@
+import '../App.css';
+
+const App = () => <div>Hello world!</div>;
+
+export default App;
